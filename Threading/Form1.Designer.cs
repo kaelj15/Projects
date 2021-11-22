084bfa9aaf87f0b4844e93b7da7bb628e0ec8812
kaelj15 <94839273+kaelj15@users.noreply.github.com>
2021-11-22 22:27:38 +1100

    Threading

---
+++ Threading/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Threading
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.tbxRed = new System.Windows.Forms.TextBox();
+            this.tbxBlue = new System.Windows.Forms.TextBox();
+            this.tbxGreen = new System.Windows.Forms.TextBox();
+            this.pbxRed = new System.Windows.Forms.PictureBox();
+            this.pbxGreen = new System.Windows.Forms.PictureBox();
+            this.pbxBlue = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGreen)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBlue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // tbxRed
+            // 
+            this.tbxRed.Location = new System.Drawing.Point(93, 15);
+            this.tbxRed.Name = "tbxRed";
+            this.tbxRed.Size = new System.Drawing.Size(100, 20);
+            this.tbxRed.TabIndex = 1;
+            // 
+            // tbxBlue
+            // 
+            this.tbxBlue.Location = new System.Drawing.Point(305, 15);
+            this.tbxBlue.Name = "tbxBlue";
+            this.tbxBlue.Size = new System.Drawing.Size(100, 20);
+            this.tbxBlue.TabIndex = 2;
+            // 
+            // tbxGreen
+            // 
+            this.tbxGreen.Location = new System.Drawing.Point(199, 15);
+            this.tbxGreen.Name = "tbxGreen";
+            this.tbxGreen.Size = new System.Drawing.Size(100, 20);
+            this.tbxGreen.TabIndex = 3;
+            // 
+            // pbxRed
+            // 
+            this.pbxRed.BackColor = System.Drawing.Color.Red;
+            this.pbxRed.Location = new System.Drawing.Point(93, 470);
+            this.pbxRed.Name = "pbxRed";
+            this.pbxRed.Size = new System.Drawing.Size(100, 10);
+            this.pbxRed.TabIndex = 4;
+            this.pbxRed.TabStop = false;
+            // 
+            // pbxGreen
+            // 
+            this.pbxGreen.BackColor = System.Drawing.Color.Green;
+            this.pbxGreen.Location = new System.Drawing.Point(199, 470);
+            this.pbxGreen.Name = "pbxGreen";
+            this.pbxGreen.Size = new System.Drawing.Size(100, 10);
+            this.pbxGreen.TabIndex = 5;
+            this.pbxGreen.TabStop = false;
+            // 
+            // pbxBlue
+            // 
+            this.pbxBlue.BackColor = System.Drawing.Color.Blue;
+            this.pbxBlue.Location = new System.Drawing.Point(305, 470);
+            this.pbxBlue.Name = "pbxBlue";
+            this.pbxBlue.Size = new System.Drawing.Size(100, 10);
+            this.pbxBlue.TabIndex = 6;
+            this.pbxBlue.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 492);
+            this.Controls.Add(this.pbxBlue);
+            this.Controls.Add(this.pbxGreen);
+            this.Controls.Add(this.pbxRed);
+            this.Controls.Add(this.tbxGreen);
+            this.Controls.Add(this.tbxBlue);
+            this.Controls.Add(this.tbxRed);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxRed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxGreen)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxBlue)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox tbxRed;
+        private System.Windows.Forms.TextBox tbxBlue;
+        private System.Windows.Forms.TextBox tbxGreen;
+        private System.Windows.Forms.PictureBox pbxRed;
+        private System.Windows.Forms.PictureBox pbxGreen;
+        private System.Windows.Forms.PictureBox pbxBlue;
+    }
+}
+
---
+++ Threading/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Threading
+{
+    public partial class Form1 : Form
+    {
+        Thread t1;
+        Thread t2;
+        Thread t3;
+
+        delegate void CTBMethod(int val);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int red = int.Parse(tbxRed.Text);
+                int green = int.Parse(tbxGreen.Text);
+                int blue = int.Parse(tbxBlue.Text);
+
+                t1 = new Thread(new ParameterizedThreadStart(LoopRed));
+                t2 = new Thread(new ParameterizedThreadStart(LoopGreen));
+                t3 = new Thread(new ParameterizedThreadStart(LoopBlue));
+
+                if (red > 430 && green > 430 && blue > 430)
+                {
+                    red = 400;
+                    green = 400;
+                    blue = 400;
+                }
+
+                t1.Start(red);
+                t2.Start(green);
+                t3.Start(blue);
+
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Not Valid Number", "Error");
+            }
+
+        }
+
+
+        public void LoopRed(object obs)
+        {
+            int x = (int)obs;
+
+            CTBMethod methodRed = new CTBMethod(UpdateRed);
+
+            for (int i = 0; i < x; i++)
+            {
+                this.Invoke(methodRed, i);
+                Thread.Sleep(2);
+            }
+        }
+
+        public void LoopGreen(object obs)
+        {
+            int x = (int)obs;
+
+            CTBMethod methodGreen = new CTBMethod(UpdateGreen);
+
+            for (int i = 0; i < x; i++)
+            {
+                this.Invoke(methodGreen, i);
+                Thread.Sleep(2);
+            }
+        }
+
+        public void LoopBlue(object obs)
+        {
+            int x = (int)obs;
+
+            CTBMethod methodBlue = new CTBMethod(UpdateBlue);
+
+            for (int i = 0; i < x; i++)
+            {
+                this.Invoke(methodBlue, i);
+                Thread.Sleep(2);
+            }
+        }
+
+        public void UpdateRed(int val)
+        {
+            pbxRed.Height = val;
+            pbxRed.Top--;
+            pbxRed.Refresh();
+        }
+
+        public void UpdateGreen(int val)
+        {
+            pbxGreen.Height = val;
+            pbxGreen.Top--;
+            pbxGreen.Refresh();
+        }
+
+        public void UpdateBlue(int val)
+        {
+            pbxBlue.Height = val;
+            pbxBlue.Top--;
+            pbxBlue.Refresh();
+        }
+
+    }
+}
